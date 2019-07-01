e755b91a60eeb1b52ee678a3463d7ad3ca291a4b
Tony Kovanen <tony.kovanen@gmail.com>
2019-07-01 12:21:23 -0700

    o

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ const IndexPage = () => (
     <p>Now go build something great. 5</p>
     <p>Now go build something great. 6</p>
     <p>Now go build something great. 2</p>
+    <p>Now go build something great. 10</p>
     <p>Now go build something great. 2</p>
     <p>Now go build something great. 2</p>
     <p>Now go build something great. 2</p>
