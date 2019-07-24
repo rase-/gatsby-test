01a648adab87fafef5c51116b37b4b7b1e2df4fa
Tony Kovanen <tony.kovanen@gmail.com>
2019-07-23 19:29:58 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ const IndexPage = () => (
     <p>{process.env.GATSBY_TEST_VAR}</p>
     <p>Now go build something great.</p>
     <p>Now go build something great. 3</p>
-    <p>Now go build something great. 2</p>
     <p>Now go build something great. 3</p>
     <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
       <Image />
