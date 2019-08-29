326a9b0447dce1afc240f34156499fcf81675821
Tony Kovanen <tony.kovanen@gmail.com>
2019-08-29 02:01:48 -0700

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import {Link} from 'gatsby';
 
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
@@ -14,9 +14,9 @@ const IndexPage = () => (
     <p>{process.env.GATSBY_TEST_VAR}</p>
     <p>Now go build something great. 1</p>
     <p>Now go build something great. 2</p>
-    <p>Now go build something great. 2</p>
     <p>Now go build something great. 3</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>Now go build something great. 4</p>
+    <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
