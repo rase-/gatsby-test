864a73077ef721219f6fbce29b06ad92102a1f4d
Tony Kovanen <tony.kovanen@gmail.com>
2019-08-29 00:34:04 -0700

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import {Link} from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people!</h2>
+    <h1>Hi people!</h1>
     <p>Welcome to your new Gatsby site. This is a build! VERSION NETLIFY</p>
     <p>Welcome to your new Gatsby site. Again.</p>
     <p>{process.env.GATSBY_TEST_VAR}</p>
     <p>Now go build something great. 1</p>
     <p>Now go build something great. 2</p>
     <p>Now go build something great. 2</p>
-    <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
+    <p>Now go build something great. 3</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
